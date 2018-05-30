2aecde7b2cdc06e342381642d0aacef44348c544
吴华林 <wuhualin@wuhualins-macbook-pro.local>
2018-05-30 23:34:27 +0800

    Initial Commit

---
+++ LoadLocalHtml/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadLocalHtml
+//
+//  Created by 吴华林 on 2018/5/30.
+//  Copyright © 2018年 吴华林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadLocalHtml/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadLocalHtml
+//
+//  Created by 吴华林 on 2018/5/30.
+//  Copyright © 2018年 吴华林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
